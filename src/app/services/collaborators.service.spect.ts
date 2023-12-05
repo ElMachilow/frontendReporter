d2ee340a7e09adaee6b4d9ea41bf5d0abdd347e4
ElMachilow <U202114983@upc.edu.pe>
2023-12-05 01:41:32 -0500

    se agrego  el reporter de mocha

--- src/app/services/collaborators.service.spect.ts
+++ src/app/services/collaborators.service.spect.ts
@@ -1,19 +1,30 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { CollaboratorsService } from './collaborators.service'; 
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CollaboratorsService } from './collaborators.service';
+import { environment } from 'environments/environment';
+import { Collaborator } from 'app/Models/collaborator.model';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+import { of } from 'rxjs';
+
 
 describe('CollaboratorsService', () => {
   let service: CollaboratorsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [CollaboratorsService],
+      providers: [CollaboratorsService]
     });
-
     service = TestBed.inject(CollaboratorsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpMock.verify();
+  });
+  
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
