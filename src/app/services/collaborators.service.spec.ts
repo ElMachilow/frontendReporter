def13a5017fad0d964983e88743791810832b69f
marcelofrancisco.nieva.contractor <marcelofrancisco.nieva.contractor@bbva.com>
2023-12-20 12:05:03 -0500

    sed agregaron cambiuos en los test

--- src/app/services/collaborators.service.spec.ts
+++ src/app/services/collaborators.service.spec.ts
@@ -6,11 +6,17 @@ import { Collaborator } from 'app/Models/collaborator.model';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 import { of } from 'rxjs';
+import { TrackPosition } from 'app/Models/trackPosition.model';
+import { ManagementArea } from 'app/Models/managementArea.model';
+import { NivelNeoris } from 'app/Models/nivelNeoris.model';
+import { PlataformaBBVA } from 'app/Models/plataformaBBVA.model';
+import { EstadoRecruiting } from 'app/Models/estadoRecruiting.model';
 
 
  describe('CollaboratorsService', () => {
   let service: CollaboratorsService;
   let httpMock: HttpTestingController;
+  const apiUrl = environment.apiUrl;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,21 +35,86 @@ import { of } from 'rxjs';
     expect(service).toBeTruthy();
   });
 
- /* it('should list users',() =>{
+ it('should list users',() =>{
+  //const apiUrl = environment.apiUrl;
+  // Definimos un array de usuarios simulados que esperamos recibir del servicio
     const mockUsers = [
       { id: 1, name: 'User1' },
       { id: 2, name: 'User2' },
     ];
-
+  // Llamamos al método listUsers del servicio. Al ser una operación 
+  //asíncrona, usamos subscribe para esperar la respuesta del observable.
     service.listUsers().subscribe((users) =>{
       expect(users).toEqual(mockUsers);
     });
+  // Utilizamos httpMock.expectOne para interceptar la solicitud HTTP
+  // realizada por el servicio y proporcionar una respuesta simulada.
+    const req = httpMock.expectOne(`${apiUrl}/colaborador`);
 
-    const req = httpMock.expectOne(`demo/colaborador`);
+  // Verificamos que la solicitud sea de tipo GET 
     expect(req.request.method).toBe('GET');
+  // Usamos req.flush para simular la respuesta del servidor con nuestros usuarios simulados
     req.flush(mockUsers);
   });
-*/
+
+  it('should get profile by XP',() =>{
+    const mockUsers = { id: 1, name: 'User1' }   
+    const xpColaborador = 'XP000001'
+    service.getProfileByXp(xpColaborador).subscribe((profile) =>{
+      expect(profile).toEqual(mockUsers);
+    });
+
+    const req = httpMock.expectOne(`${apiUrl}/colaborador/profile/`+xpColaborador);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers); 
+  });
+
+  it('should create a collaborator',() =>{
+    const mockCollaborator: Collaborator = 
+    {
+      id: 1,
+      nombres: 'John Doe',
+      apellidos: 'nieva',
+      codigoEmpleado: 'asasd',
+      codigoXp: 'asd',
+      pais: 'as',
+      dni: 'asd',
+      estadoNeoris: 'asd',
+      fechaNacimiento: 'asd',
+      celular: 'asd',
+      fechaIngresoNeoris: 'asd',
+      fechaSalidaNeoris: 'asd',
+      correoNeoris: 'asd',
+      estadoPmoBbva: 'asd',
+      correoContractor: 'asd',
+      fechaAltaContractor: 'asd',
+      vdiPmo: 'asd',
+      fechaAltaVDI: 'asd',
+      accesoServiceNow: 'asd',
+      fechaAltaAccesoServiceNow: 'asd',
+      timeReport: 'asd',
+      accesoRacf: 'asd',
+      fechaAccesoRacf: 'asd',
+      ipVdiPmo: 'asd',
+      trackPosition: new TrackPosition,
+      managementArea: new ManagementArea,
+      nivelNeoris: new NivelNeoris,
+      plataformaBBVA: new PlataformaBBVA,
+      estadoRecruiting: new EstadoRecruiting
+    };
+
+    service.createCollaboratos(mockCollaborator).subscribe((collaborator) => {
+      expect(collaborator).toEqual(mockCollaborator);
+    });
+
+    const req = httpMock.expectOne(`${apiUrl}/colaborador`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(mockCollaborator);
+    req.flush(mockCollaborator);
+    
+  });
+
+  
   describe('test for getValue', () =>{
     //AAA
     it('should return "my value"', () =>{
